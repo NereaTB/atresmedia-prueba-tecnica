283602fbe142d28bb39b21b3ab0a2eae9f8f5d51
Nerea <163418422+NereaTovar@users.noreply.github.com>
2024-07-30 20:59:50 +0200

    Create input and display images

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,27 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [breeds, setBreeds] = useState();
+  const [breeds, setBreeds] = useState<string[]>([]);
+  const [selectedBreed, setSelectedBreed] = useState<string>();
+  const [selectedImages, setSelectedImages] = useState<string[]>();
 
- 
   const fetchImagesFromBreed = async (selectedBreed: string) => {
-    try{
-      const response = await fetch(`https://dog.ceo/api/breed/${selectedBreed}/images`)
+    try {
+      const response = await fetch(
+        `https://dog.ceo/api/breed/${selectedBreed}/images`
+      );
 
       if (!response.ok) {
         throw new Error(`Status: ${response.status}`);
       }
       const result = await response.json();
-
-    }catch{
-
+      setSelectedImages(result.message);
+    } catch {
+      throw new Error("Las imágenes paginadas no han podido ser cargadas");
     }
-  }
+  };
 
- const fetchBreeds = async () => {
+  const fetchBreeds = async () => {
     try {
       const response = await fetch("https://dog.ceo/api/breeds/list/all");
 
@@ -27,21 +30,46 @@ function App() {
         throw new Error(`Status: ${response.status}`);
       }
       const result = await response.json();
-      setBreeds(result.message)
-      
+      const breedsArray = Object.keys(result.message);
+      setBreeds(breedsArray);
     } catch {
       throw new Error("Las razas no han podido ser cargadas");
     }
   };
 
+  const handleSelectBreed = (event) => {
+    setSelectedBreed(event.target.value);
+  };
 
   useEffect(() => {
     fetchBreeds();
   }, []);
 
-  https: return (
-    <>      
-    </>
+  useEffect(() => {
+    if (selectedBreed) {
+      fetchImagesFromBreed(selectedBreed);
+    }
+  }, [selectedBreed]);
+
+  return (
+    <div>
+      {
+        <select name="breeds" onChange={handleSelectBreed}>
+          {breeds.map((breed) => {
+            return (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            );
+          })}
+        </select>
+      }
+      <section>
+        {selectedImages?.map((image) => {
+          return <img key={image} src={image} />;
+        })}
+      </section>
+    </div>
   );
 }
 
